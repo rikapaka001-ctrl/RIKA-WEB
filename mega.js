3d4f6cfe637c99884cb811893735f60fdb7fc8ff
rikapaka001-ctrl <rikapaka001@gmail.com>
2026-05-24 20:39:06 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -12,7 +12,7 @@ import * as mega from 'megajs';
 
 // Mega authentication credentials
 const auth = {
-    email: 'pakayaxmd@gmail.com', // Replace with your Mega email
+    email: 'cybershamiya001@gmail.com', // Replace with your Mega email
     password: 'lasi@#321', // Replace with your Mega password
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
